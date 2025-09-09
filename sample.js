fd3b96bc56823a9577ebaff3e1bf3639f9c89912
adhvaithprasad <adhvaithprasad@gmail.com>
2025-09-09 23:11:07 +0530

    v2

--- sample.js
+++ sample.js
@@ -2,7 +2,7 @@ var data = [
   {
     "Name": "Agiesh Kumar",
     "Batch": "1999",
-    "Department/Course": "MSc. Biotechnology",
+    "course": "MSc. Biotechnology",
     "Job": "Scientist",
     "Company": "Institute of Advanced Virology",
     "linkedin": "https://www.linkedin.com/in/agiesh-kumar-550a4921"
@@ -10,7 +10,7 @@ var data = [
   {
     "Name": "Girish  Rasineni",
     "Batch": "2005",
-    "Department/Course": "MSc.Life Sciences",
+    "course": "MSc.Life Sciences",
     "Job": "Principal Scientific Manager",
     "Company": "Aragen Life Sciences",
     "linkedin": "https://www.linkedin.com/in/girish-rasineni-b625a15"
@@ -18,7 +18,7 @@ var data = [
   {
     "Name": "Subhashree Devasenapathy",
     "Batch": "2004",
-    "Department/Course": "MSc.Biotechnology",
+    "course": "MSc.Biotechnology",
     "Job": "Functional Head",
     "Company": "Wipro",
     "linkedin": "https://www.linkedin.com/in/subhashree-devasenapathy-ph-d-85681223?"
@@ -26,7 +26,7 @@ var data = [
   {
     "Name": "Sudalai Subramani",
     "Batch": "2008",
-    "Department/Course": "M.Tech Environmental Eng",
+    "course": "M.Tech Environmental Eng",
     "Job": "Assistant Professor",
     "Company": "Pondicherry University",
     "linkedin": "https://www.linkedin.com/in/sudalai-subramani-51525345"
@@ -34,7 +34,7 @@ var data = [
   {
     "Name": "Sindhuja Mohan",
     "Batch": "2023",
-    "Department/Course": "MBA",
+    "course": "MBA",
     "Job": "Project Accountant R&D",
     "Company": "Pondicherry University",
     "linkedin": "https://www.linkedin.com/in/sindhujamohan13?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2BRJfEzuXT2u8wIeIO4BpsQ%3D%3D"
@@ -42,7 +42,7 @@ var data = [
   {
     "Name": "Srija",
     "Batch": "2019",
-    "Department/Course": "MD - Pharmacology",
+    "course": "MD - Pharmacology",
     "Job": "Drug safety specialist",
     "Company": "Accenture",
     "linkedin": "https://www.linkedin.com/in/dr-srija-g-b467431b3?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bm%2FcoNCgESJCKd4wHSItDKw%3D%3D"
@@ -50,7 +50,7 @@ var data = [
   {
     "Name": "Dr. Pragati",
     "Batch": "2022",
-    "Department/Course": "PhD Physics",
+    "course": "PhD Physics",
     "Job": "Researcher and Brain signal analyst",
     "Company": "Pondicherry University",
     "linkedin": "https://www.linkedin.com/in/dr-pragati-patel-8076631b8?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BAkHrTpzISbeZAgQHkIzlEg%3D%3D"
@@ -58,7 +58,7 @@ var data = [
   {
     "Name": "Dr Anousouya",
     "Batch": "2020",
-    "Department/Course": "PhD Computer Science",
+    "course": "PhD Computer Science",
     "Job": "Guest Faculty",
     "Company": "Pondicherry University",
     "linkedin": "https://www.linkedin.com/in/dr-anousouya-devi-52795823b?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BCg9Z%2FLsfT62K0s23qP46Lw%3D%3D"
@@ -66,7 +66,7 @@ var data = [
   {
     "Name": "Kailas",
     "Batch": "2024",
-    "Department/Course": "MA. History",
+    "course": "MA. History",
     "Job": "Field Investigator",
     "Company": "Pondicherry University",
     "linkedin": "https://www.linkedin.com/in/kailas-v-j-940824172?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BFX7SN35ISl%2B8IktJSIzjoQ%3D%3D"
@@ -74,7 +74,7 @@ var data = [
   {
     "Name": "Kuppusamy Sakthivel",
     "Batch": "2024",
-    "Department/Course": "MBA",
+    "course": "MBA",
     "Job": "Chief Executive Officer",
     "Company": "Milky Haven",
     "linkedin": "https://www.linkedin.com/in/kuppusamy-sakthivel?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B3H1TTJS9R96MT76pJecpvg%3D%3D"
@@ -82,7 +82,7 @@ var data = [
   {
     "Name": "Vijayavendan",
     "Batch": "2014",
-    "Department/Course": "B.Tech ECE",
+    "course": "B.Tech ECE",
     "Job": "Senior Customer Engineer",
     "Company": "Datamatics",
     "linkedin": "https://www.linkedin.com/in/vijayavendan-s-bb803798?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BS9KpeCYZQzSWSH77ZoUh3g%3D%3D"
@@ -90,7 +90,7 @@ var data = [
   {
     "Name": "Thiruarangam Devarajan",
     "Batch": "2006",
-    "Department/Course": "MSc.Life Sciences",
+    "course": "MSc.Life Sciences",
     "Job": "Deputy General Manager",
     "Company": "R1 RCM",
     "linkedin": "https://www.linkedin.com/in/thiruarangam-devarajan-b0b66615?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BtTxBm0hPQiOSxgbqKPWSCQ%3D%3D"
@@ -98,7 +98,7 @@ var data = [
   {
     "Name": "Selvakumar Edwardraja",
     "Batch": "2003",
-    "Department/Course": "P.G. Bioinformatics",
+    "course": "P.G. Bioinformatics",
     "Job": "Protein design & Engineering Scientist",
     "Company": "AdvanCell",
     "linkedin": "https://www.linkedin.com/in/selvakumaredwardraja?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BySj3MEBPSHGsKHfn2Oq%2BuQ%3D%3D"
@@ -106,7 +106,7 @@ var data = [
   {
     "Name": "Narayan Kesavan",
     "Batch": "2006",
-    "Department/Course": "MCA",
+    "course": "MCA",
     "Job": "Project Manager",
     "Company": "Radian Generation",
     "linkedin": "https://www.linkedin.com/in/narayan-kesavan?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B9xg0ODucRfqQd0XVvc2kKA%3D%3D"
@@ -114,7 +114,7 @@ var data = [
   {
     "Name": "Sri Barani",
     "Batch": "2024",
-    "Department/Course": "Master of Library&IS",
+    "course": "Master of Library&IS",
     "Job": "Library Professional",
     "Company": "NIT Trichy",
     "linkedin": "https://www.linkedin.com/in/sri-barani-r-40a9b3283?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BdKtrLykOR2GV6Kb0iUNhVQ%3D%3D"
@@ -122,7 +122,7 @@ var data = [
   {
     "Name": "Santhiya",
     "Batch": "2023",
-    "Department/Course": "MBA",
+    "course": "MBA",
     "Job": "Human Resource Analyst",
     "Company": "Unisys",
     "linkedin": "https://www.linkedin.com/in/santhiya-mourthy?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BPzBy6BdfQIOvH6L8I9SPiQ%3D%3D"
@@ -130,7 +130,7 @@ var data = [
   {
     "Name": "Lavanya",
     "Batch": "2022",
-    "Department/Course": "Masters Bioinformatics",
+    "course": "Masters Bioinformatics",
     "Job": "Human Resource Executive",
     "Company": "Bytive",
     "linkedin": "https://www.linkedin.com/in/lavanya-b12k?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BXSappcR3QpS131tRZhxxhA%3D%3D"
@@ -138,7 +138,7 @@ var data = [
   {
     "Name": "Manikandan",
     "Batch": "2012",
-    "Department/Course": "M.S.W. Human Resources",
+    "course": "M.S.W. Human Resources",
     "Job": "Lead Manager - HR Operations",
     "Company": "Lumina Datamatics Limited",
     "linkedin": "https://www.linkedin.com/in/manikandan-elango-b9b38436?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BrifPbRE7SoqgKGeBCXgQ2w%3D%3D"
@@ -146,7 +146,7 @@ var data = [
   {
     "Name": "Priyanka Venkatachalabathi",
     "Batch": "2015",
-    "Department/Course": "MCA",
+    "course": "MCA",
     "Job": "Project Manager",
     "Company": "TNQTech",
     "linkedin": "https://www.linkedin.com/in/priyanka-venkatachalabathi?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2BrZRFfweQrOy62cvt6WlAw%3D%3D"
@@ -154,7 +154,7 @@ var data = [
   {
     "Name": "Ravi Kiran",
     "Batch": "2011",
-    "Department/Course": "M.Tech in Network and Internet Eng",
+    "course": "M.Tech in Network and Internet Eng",
     "Job": "Lead Engineer",
     "Company": "Altran",
     "linkedin": "https://www.linkedin.com/in/ravi-kiran-b6b0731b?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2BDCFWsztQmmS7cvMqdaE8A%3D%3D"
@@ -162,7 +162,7 @@ var data = [
   {
     "Name": "Kalidas Chakravarthy",
     "Batch": "2003",
-    "Department/Course": "B.Tech ECE",
+    "course": "B.Tech ECE",
     "Job": "Senior Business Program Manager",
     "Company": "Microsoft",
     "linkedin": "https://www.linkedin.com/in/kalidasg?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BVfNv8V%2BGTfKF%2Bw3UvJIUCw%3D%3D"
@@ -170,7 +170,7 @@ var data = [
   {
     "Name": "Ajees Kumar",
     "Batch": "2025",
-    "Department/Course": "PhD, Bioinformatics",
+    "course": "PhD, Bioinformatics",
     "Job": "Reaseach Scholar",
     "Company": "Pondicherry University",
     "linkedin": "https://www.linkedin.com/in/ajzeeskumar11?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BYhS%2FVwlZT3ClxQKClG4Teg%3D%3D"
@@ -178,7 +178,7 @@ var data = [
   {
     "Name": "Saranya Sankar",
     "Batch": "2018",
-    "Department/Course": "MBA",
+    "course": "MBA",
     "Job": "Manager - Talent Acquisition",
     "Company": "TCS",
     "linkedin": "https://www.linkedin.com/in/saranya-sankar-24110a163?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BN8M3DMatR4m47ZWRgc8lug%3D%3D"
@@ -186,7 +186,7 @@ var data = [
   {
     "Name": "Reshma Raveendran",
     "Batch": "2013",
-    "Department/Course": "Master's Degree, Physics",
+    "course": "Master's Degree, Physics",
     "Job": "Organic Electronics Researcher",
     "Company": "Shiv Nadar Institution",
     "linkedin": "https://www.linkedin.com/in/reshma-raveendran-985a3768?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B8ErFWUg5TLOKT%2B68z8YAmQ%3D%3D"
@@ -194,7 +194,7 @@ var data = [
   {
     "Name": "Saurabh Suman Jha",
     "Batch": "2015",
-    "Department/Course": "MCA",
+    "course": "MCA",
     "Job": "Lead Software Engineer",
     "Company": "Publicis Sapient",
     "linkedin": "https://www.linkedin.com/in/skslmcj?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bnkh7E%2BsHSz%2BVyoK%2Fznf1lA%3D%3D"
@@ -202,7 +202,7 @@ var data = [
   {
     "Name": "Jeevani Konda",
     "Batch": "2016",
-    "Department/Course": "M.Tech ECE",
+    "course": "M.Tech ECE",
     "Job": "Senior Engineer",
     "Company": "L&T Technology Services",
     "linkedin": "https://www.linkedin.com/in/jeevanikonda?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B6GlLvKwDS3e9Vjvz35ATNA%3D%3D"
@@ -210,7 +210,7 @@ var data = [
   {
     "Name": "Karthick Rajaram",
     "Batch": "2021",
-    "Department/Course": "MBA",
+    "course": "MBA",
     "Job": "Software Engineer",
     "Company": "Boeing",
     "linkedin": "https://www.linkedin.com/in/karthickrajaram?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BQcOKuddAQdSmDBlBPyMmEA%3D%3D"
@@ -218,7 +218,7 @@ var data = [
   {
     "Name": "Anshul",
     "Batch": "2021",
-    "Department/Course": "MCA",
+    "course": "MCA",
     "Job": "Software Engineer",
     "Company": "Nutanix",
     "linkedin": "https://www.linkedin.com/in/anshul-r-63074026a?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BQLpxq4nkRGmsWI1O%2BxkY1g%3D%3D"
@@ -226,7 +226,7 @@ var data = [
   {
     "Name": "Krishnaraj Kannan",
     "Batch": "2025",
-    "Department/Course": "MBA",
+    "course": "MBA",
     "Job": "Software Engineer",
     "Company": "CognitiveValueTech",
     "linkedin": "https://www.linkedin.com/in/krishnaraj-kannan-5a0a4b236?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BjHbgi8BcQcOedQkjYlE4Zw%3D%3D"
@@ -234,7 +234,7 @@ var data = [
   {
     "Name": "Santhosh",
     "Batch": "2023",
-    "Department/Course": "B.Tech ECE",
+    "course": "B.Tech ECE",
     "Job": "Design Engineer",
     "Company": "Aktis Engineering Solutions",
     "linkedin": "https://www.linkedin.com/in/santhosh-t-00945a311?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BLMjj05yiSBSjdeL8ob%2FNrQ%3D%3D"
@@ -242,7 +242,7 @@ var data = [
   {
     "Name": "Mohammed Ashif",
     "Batch": "2022",
-    "Department/Course": "BBA",
+    "course": "BBA",
     "Job": "Software Test Engineer",
     "Company": "IINVSYS",
     "linkedin": "https://www.linkedin.com/in/mohammed-ashif-0031b1192?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BEVJYBWNcRu6tt9vWwIzxiw%3D%3D"
@@ -250,7 +250,7 @@ var data = [
   {
     "Name": "Yuvaraj",
     "Batch": "2022",
-    "Department/Course": "MCA",
+    "course": "MCA",
     "Job": "Software Developer",
     "Company": "Sun Direct TV PVT",
     "linkedin": "https://www.linkedin.com/in/yuvaraj-k-a467a2313?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BGLFp6pRHS2e6EDbZUsGPZA%3D%3D"
@@ -258,7 +258,7 @@ var data = [
   {
     "Name": "Susheel Chandy",
     "Batch": "2014",
-    "Department/Course": "MSc. Bioinformatics",
+    "course": "MSc. Bioinformatics",
     "Job": "Associate Lead",
     "Company": "Cellworks",
     "linkedin": "https://www.linkedin.com/in/sgchandy404?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BvO%2FhgXRHSRa83cGvMgk74g%3D%3D"
@@ -266,7 +266,7 @@ var data = [
   {
     "Name": "Fathimath Sana",
     "Batch": "2022",
-    "Department/Course": "MCA",
+    "course": "MCA",
     "Job": "Software Developer",
     "Company": "Meridian Solutions",
     "linkedin": "https://www.linkedin.com/in/fathimath-sana-871210254?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BG7vFi4SRReuIQMVb7jlkIw%3D%3D"
@@ -274,7 +274,7 @@ var data = [
   {
     "Name": "Hariharan",
     "Batch": "2023",
-    "Department/Course": "MCA",
+    "course": "MCA",
     "Job": "Junior Software Engineer",
     "Company": "Enterprise Minds",
     "linkedin": "https://www.linkedin.com/in/hariharan-murugan?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bl8P%2FP7rISW20h%2Fyll7QvuQ%3D%3D"
@@ -282,7 +282,7 @@ var data = [
   {
     "Name": "Ilackya Bhaskar",
     "Batch": "2018",
-    "Department/Course": "MSc. Computer Science",
+    "course": "MSc. Computer Science",
     "Job": "Software Associate",
     "Company": "Cognizant",
     "linkedin": "https://www.linkedin.com/in/ilackya-bhaskar-6836a9145?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BmqunXD1JQMO%2BmxVkO1qhXw%3D%3D"
@@ -290,7 +290,7 @@ var data = [
   {
     "Name": "Ramakrishnan Tamilarasan",
     "Batch": "2021",
-    "Department/Course": "B.Tech ECE",
+    "course": "B.Tech ECE",
     "Job": "Service Engineer",
     "Company": "Oracle",
     "linkedin": "https://www.linkedin.com/in/ramakrishnan-tamilarasan-ba1816193?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BKyB6M7qaSQmkkfTlf4Qi3Q%3D%3D"
@@ -298,7 +298,7 @@ var data = [
   {
     "Name": "Boaz John",
     "Batch": "2014",
-    "Department/Course": "M.Tech CSE",
+    "course": "M.Tech CSE",
     "Job": "Software Engineer",
     "Company": "Juspay Technology",
     "linkedin": "https://www.linkedin.com/in/boaz-john-b1394020?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BLpGzgE3KREmgj4DNZcW6wg%3D%3D"
@@ -306,7 +306,7 @@ var data = [
   {
     "Name": "Murugan Baskaran",
     "Batch": "2011",
-    "Department/Course": "B.Tech ICE",
+    "course": "B.Tech ICE",
     "Job": "Deputy Manager",
     "Company": "Steril-Gene Life Sciencess",
     "linkedin": "https://www.linkedin.com/in/murugan-baskaran-a429a4102?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BhUfbbFbvTl6QkR9soPAx4g%3D%3D"
@@ -314,7 +314,7 @@ var data = [
   {
     "Name": "Satyanarayana Medisetti",
     "Batch": "2013",
-    "Department/Course": "Diploma in CSE",
+    "course": "Diploma in CSE",
     "Job": "Software Developer",
     "Company": "IDC Technologies Solutions",
     "linkedin": "https://www.linkedin.com/in/satyanarayana-medisetti-00b58b158?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bwbw7H4AMRnuJ%2F0DgeglxCA%3D%3D"
